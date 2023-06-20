cdbae1481304cd66b1ae39bd891a2e48ab96beac
LowellC86 <lowellcaraballo@gmail.com>
2023-06-20 18:35:58 -0400

    done son

--- korilla/src/App.jsx
+++ korilla/src/App.jsx
@@ -1,35 +1,123 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Receipts from './components/Receipts'
 import './App.css'
 
+const receipts = [
+  {
+    id: 1,
+    person: 'Jeremy',
+    order: {
+      main: 'Burrito',
+      protein: 'Skirt Steak',
+      rice: 'Purple Rice',
+      sauce: 'Green Crack',
+      toppings: ['Baby Bok Choy','Cucumber Kimchi'],
+      drink: 'Thai Iced Tea',
+      cost: 22,
+    },
+    paid: false,
+  },
+  {
+    id: 2,
+    person: 'Tiffany',
+    order: {
+      main: 'Rice Bowl',
+      protein: 'Ginger Chicken',
+      rice: 'Sticky Rice',
+      sauce: 'Korilla',
+      toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+      drink: 'Korchata',
+      cost: 19,
+    },
+    paid: false,
+  },
+  {
+    id: 3,
+    person: 'Brittany',
+    order: {
+      main: 'Salad Bowl',
+      protein: 'Organic Tofu',
+      rice: 'none',
+      sauce: "K'lla",
+      toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+      drink: 'Sparkling Blood Orange Soda',
+      cost: 20,
+    },
+    paid: false,
+  },
+  {
+    id: 4,
+    person: 'Tylus',
+    order: {
+      main: 'Burrito Bowl',
+      protein: 'Carnitas',
+      rice: 'yellow rice',
+      sauce: "Jalapeno Honey",
+      toppings: ['Grilled Corn', 'Pico De Gallo', 'Guacamole'],
+      drink: 'Pineapple Jarritos',
+      cost: 23,
+    },
+    paid: false,
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Korilla</h1>
+      </header>
+    <div className='receipts-info'>
+        <Receipts
+          person = {receipts[0].person}
+          main = {receipts[0].order.main}
+          protein = {receipts[0].order.protein}
+          rice = {receipts[0].order.rice}
+          sauce = {receipts[0].order.sauce}
+          toppings = {receipts[0].order.toppings}
+          drink = {receipts[0].order.drink}
+          cost = {receipts[0].order.cost}
+          paid = {receipts[0].paid}
+        />
+        <Receipts
+          person = {receipts[1].person}
+          main = {receipts[1].order.main}
+          protein = {receipts[1].order.protein}
+          rice = {receipts[1].order.rice}
+          sauce = {receipts[1].order.sauce}
+          toppings = {receipts[1].order.toppings}
+          drink = {receipts[1].order.drink}
+          cost = {receipts[1].order.cost}
+          paid = {receipts[1].paid}
+        />
+        <Receipts
+          person = {receipts[2].person}
+          main = {receipts[2].order.main}
+          protein = {receipts[2].order.protein}
+          rice = {receipts[2].order.rice}
+          sauce = {receipts[2].order.sauce}
+          toppings = {receipts[2].order.toppings}
+          drink = {receipts[2].order.drink}
+          cost = {receipts[2].order.cost}
+          paid = {receipts[2].paid}
+        />
+        <Receipts
+          person = {receipts[3].person}
+          main = {receipts[3].order.main}
+          protein = {receipts[3].order.protein}
+          rice = {receipts[3].order.rice}
+          sauce = {receipts[3].order.sauce}
+          toppings = {receipts[3].order.toppings}
+          drink = {receipts[3].order.drink}
+          cost = {receipts[3].order.cost}
+          paid = {receipts[3].paid}
+        />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
+    )    
+  }
 export default App
---
+++ korilla/src/components/UnPaid.jsx
@@ -0,0 +1,7 @@
+export default function UnPaid () {
+  return (
+    <div>
+      <h2>No Food For You!</h2>
+    </div>
+  )
+}
